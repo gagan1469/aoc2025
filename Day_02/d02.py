c29be7186548f2ae9e4ef9c41b1663cff1af7e03
Gagan Singh <Gagan_Singh@oxy.com>
2025-12-02 23:50:35 -0600

    Day 2 solution

--- Day_02/d02.py
+++ Day_02/d02.py
@@ -1,3 +1,5 @@
+import re
+
 # read the inputs
 def load_data_set(data_file: str, delimiter: str = '\n') -> list:
     with open(data_file, 'r') as f:
@@ -27,6 +29,32 @@ def find_invalid_product_codes(code_range: str) -> tuple[list, int]:
 
     return invalid_codes, invalid_code_count
 
+def find_invalid_product_codes_2(code_range: str) -> tuple[list, int]:
+    id_set = code_range.split(sep='-')
+    first_id = int(id_set[0])
+    last_id = int(id_set[1])
+
+    # print(first_id, last_id)
+
+    invalid_codes = []
+    invalid_code_count = 0
+    for id in range(first_id, last_id+1):
+        code = str(id)
+        mid = len(code) // 2
+        for i in range(1, mid+1):
+            pattern = code[:i]
+            matches = re.findall(pattern=pattern, string=code)
+            concatenated_matches = ''.join(matches)
+            if len(concatenated_matches) == len(code):
+                # invalid id found
+                # print(concatenated_matches, code)
+                # 222222 can be added multiple times. avoid it.
+                if id not in invalid_codes:
+                    invalid_code_count += 1
+                    invalid_codes.append(id)
+
+    return invalid_codes, invalid_code_count
+
 def main():
     fname = 'Day_02\inputs.txt'
     # fname = 'Day_02\sample_inputs.txt'
@@ -36,8 +64,8 @@ def main():
 
     invalid_total = 0
     for line in lines:
-        invalid_codes, count = find_invalid_product_codes(code_range=line)
-        # print(count, invalid_codes)
+        invalid_codes, count = find_invalid_product_codes_2(code_range=line)
+        print(count, invalid_codes)
 
         if count > 0:
             for code in invalid_codes:
