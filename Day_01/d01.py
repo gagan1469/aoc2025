8587d9a96aa12f444c2d5044fb32af261ad87d2f
Gagan Singh <Gagan_Singh@oxy.com>
2025-12-02 21:55:01 -0600

    Setup day 01 solution

---
+++ Day_01/d01.py
@@ -0,0 +1,16 @@
+# read the inputs
+def load_data_set(data_file: str, delimiter: str = '\n') -> list:
+    with open(data_file, 'r') as f:
+        lines = f.read().split(delimiter)
+
+    return lines
+
+def main():
+    fname = 'Day_01\inputs.txt'
+    fname = 'Day_01\sample_inputs.txt'
+
+    lines = load_data_set(data_file=fname)
+    print(lines)
+
+if __name__ == '__main__':
+    main()
